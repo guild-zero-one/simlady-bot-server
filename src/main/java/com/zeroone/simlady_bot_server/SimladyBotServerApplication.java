8719528ae30593e23513d9d8f9662ac586bd6fe3
GustavoLuzz <161068517+GustavoLuzz@users.noreply.github.com>
2025-04-27 17:56:52 -0300

    "feat :tada: spring inital"

---
+++ src/main/java/com/zeroone/simlady_bot_server/SimladyBotServerApplication.java
@@ -0,0 +1,13 @@
+package com.zeroone.simlady_bot_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimladyBotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimladyBotServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeroone/simlady_bot_server/SimladyBotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeroone.simlady_bot_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimladyBotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
